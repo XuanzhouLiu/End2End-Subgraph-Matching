46173b45b72bc76aa1da7209dc669ef885dd9648
Liuxz PC <liuxz17@mails.tsinghua.edu.cn>
2022-07-04 23:53:31 +0800

    Add Isonet Model

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 from neuro_match import OrderEmbedder
 from e2e_model import E2eModel
 from graphsim import SimGNN
+from iso_net import ISONET
 from dataset import SynDataset
 import torch
 import torch.nn.functional as F
@@ -17,7 +18,7 @@ import pytorch_lightning as pl
 feat_dim = 10
 hidden_dim = 2*feat_dim
 root = "Exp"
-
+device = "cuda"
 num_layers = 2
 
 from torch_geometric.loader import DataLoader
@@ -29,12 +30,13 @@ dataset = SynDataset(root, size=100, positive=50, graph_type=["ER","WS"], graph_
 loader = DataLoader(dataset, batch_size=10, follow_batch=['x_s', 'x_t'], shuffle=True)
 
 #model = E2eModel(feat_dim, hidden_dim)
-model = OrderEmbedder(2,feat_dim,hidden_dim,0.1)
+#model = OrderEmbedder(2,feat_dim,hidden_dim,0.1)
 #model = SimGNN(2, feat_dim, hidden_dim, hidden_dim*2, hidden_dim)
+model = ISONET(feat_dim, hidden_dim, hidden_dim, hidden_dim)
 
 opt = torch.optim.Adam(model.parameters(),lr = 3e-4)
 
 train_loader = loader
 
-trainer = pl.Trainer(max_epochs=100000)
+trainer = pl.Trainer(gpus = 1, max_epochs=100000)
 trainer.fit(model, train_loader, train_loader)
